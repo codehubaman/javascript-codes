517d82a0eadd914aeb3711d38f9fd829268881e0
codehubaman <amanpatel772003@gmail.com>
2025-01-31 12:47:51 +0530

    promise_async_await

--- 09_promises/promise_01.js
+++ 09_promises/promise_01.js
@@ -1,15 +1,17 @@
-
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+// promise takes  function and return object
+const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         console.log('Hello from the promise');
         resolve();
     }, 2000);
-}).then(() => {
+})
+const res = promise.then(() => {
 
     console.log('Promise Resolved');
 
 }).catch((error) => {
     console.log('Promise rejected:', error);
 });
+
 
 
